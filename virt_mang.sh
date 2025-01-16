94a0405720dedefff07e07ddf6512346e2781ac6
saeed amiri <sdramiri952@gmail.com>
2025-01-16 09:48:51 +0100

    echo a message

--- virt_mang.sh
+++ virt_mang.sh
@@ -7,3 +7,8 @@ echo yes | sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridg
 
 # Enable and start libvirtd
 sudo systemctl enable --now libvirtd
+
+# Print in red color
+echo -e "\e[31m"
+echo "Please reboot your system to complete the installation"
+echo -e "\e[0m"
